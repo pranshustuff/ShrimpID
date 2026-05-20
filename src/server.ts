b266c1e8cbcdf23c24e3b1bb715b5b4faa125a1d
pranshustuff <pranshustuff@protonmail.com>
2026-05-20 17:52:50 +0530

    Signup and login with hash

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,63 @@
 import Fastify from "fastify"
+import bcrypt from "bcrypt"
 
-const app = Fastify();
+const app = Fastify()
 
-type LoginBody = {
-    user: string,
-    pass: string
-};
+type Credentials = {
+    username: string,
+    password: string
+}
+
+type StoredUser = {
+    username: string,
+    password: string
+}
+
+const users: StoredUser[] = []
 
 app.get("/", async() => {
-    return {hello: "world"};
-});
+    return {hello: "world"}
+})
 
-app.post("/login", async (req, reply) => {
-    const data = req.body as LoginBody;
+app.post("/signup", async (req, res) => {
+    const {username, password} = req.body as Credentials
+    const hash = await bcrypt.hash(password, 11)
 
-    if (data.user == "Hello" && data.pass == "hi"){
-        return {status: "Success"};
-    } else {
-        return {status: "Failed"};
+    users.push({
+        username, 
+        password:hash
+    })
+    
+    console.log("User Saved.")
+    res.send('ok')
+})
+
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body as Credentials
+
+    const user = users.find(u => u.username == username)
+    if(!user){
+        res.send("No user found.")
+        return
     }
-});
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if(!isValid){
+        res.send("Wrong Password")
+        return
+    }
+
+    res.send('ok')
+})
 
 const start = async () => {
     try {
-        await app.listen({port:3000});
-        console.log("Server Running on port 3000");
+        await app.listen({port:3000})
+        console.log("Server Running on port 3000")
     } catch(err) {
-        console.log(err);
-        process.exit(1);
+        console.log(err)
+        process.exit(1)
     }
-};
+}
 
-start();
+start()
