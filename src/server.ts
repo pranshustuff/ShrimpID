a6791f5d313a583467a077696a3e3f126ac048ba
pranshustuff <pranshustuff@protonmail.com>
2026-05-21 14:49:12 +0530

    v1: JWT based Auth flow

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import Fastify from "fastify"
+import Fastify, { fastify } from "fastify"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const app = Fastify()
 
@@ -14,6 +17,8 @@ type StoredUser = {
 }
 
 const users: StoredUser[] = []
+
+const secret = process.env.ACCESS_TOKEN_SECRET as string
 
 app.get("/", async() => {
     return {hello: "world"}
@@ -47,7 +52,37 @@ app.post("/login", async (req, res) => {
         return
     }
 
-    res.send('ok')
+    const token = jwt.sign({username:username}, secret, {expiresIn: 60*60})
+
+    res.send({accessToken: token})
+})
+
+interface AuthRequest extends Fastify.FastifyRequest {
+    user?: {
+        username: string
+        iat: number
+        exp: number
+    }
+}
+
+async function authenticateToken(req: AuthRequest, res: Fastify.FastifyReply): Promise<void> {
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token==null) return res.status(401).send("Unauthorized")
+    
+    try {
+        const user = jwt.verify(token, secret) as AuthRequest["user"]
+        req.user = user
+    } catch(err) {
+        return res.status(403).send("Forbidden")
+    }
+}
+
+app.get("/me", async(req: AuthRequest, res) => {
+    await authenticateToken(req, res)
+    if(res.statusCode >= 400) return
+    if(!req.user) return res.status(401).send("Unauthorized")
+    res.send({username: req.user.username})
 })
 
 const start = async () => {
